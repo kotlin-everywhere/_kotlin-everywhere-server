1ce1c16b8f6bdf493bace3f54530d9b92af65fbf
Sung Min Park <vamf12@Sungs-MacBook-Pro.local>
2017-05-28 15:55:01 +0900

    사용하지 않는 initialized 삭제

--- src/main/kotlin/minek/kotlin/everywhere/server/Container.kt
+++ src/main/kotlin/minek/kotlin/everywhere/server/Container.kt
@@ -1,12 +1,5 @@
 package minek.kotlin.everywhere.server
 
 abstract class Container {
-    private var initialized = false
     fun <P, R> f() = Handler<P, R>()
-
-    companion object {
-        fun initialize(container: Container) {
-            container.initialized = true
-        }
-    }
 }
